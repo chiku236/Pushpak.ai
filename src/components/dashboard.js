7118bca0dbbe1bde530ed7190e30865caf63cd7b
arpanbarik <arpanbarik@credenceanalytics.com>
2022-05-14 16:07:27 +0530

    first

---
+++ src/components/dashboard.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+function Dashboard() {
+    return (
+
+        <div className = "table">
+            <Table striped bordered hover id="customers">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Id</th>
+                        <th>Billing Name</th>
+                        <th>Amount</th>
+                        <th>Order Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto markhoff</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+
+
+
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default Dashboard;
